1f27c59eff6354377cc3b94c1ca9f6616d7b8d39
makacies <barberrywork@gmail.com>
2024-06-07 21:33:54 +0300

    Create InvertBinaryTree.cs

---
+++ LeetCode/InvertBinaryTree.cs
@@ -0,0 +1,34 @@
+﻿// 226. Invert Binary Tree
+
+namespace LeetCode
+{
+    internal class InvertBinaryTree
+    {
+        internal TreeNode InvertTree(TreeNode root)
+        {
+            Swap(root);
+            return root;
+
+            void Swap(TreeNode? node)
+            {
+                if (node == null)
+                {
+                    return;
+                }
+
+                (node.left, node.right) = (node.right, node.left);
+                Swap(node.left);
+                Swap(node.right);
+            }
+        }
+
+    }
+
+
+    public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+    {
+        public int val = val;
+        public TreeNode? left = left;
+        public TreeNode? right = right;
+    }
+}
