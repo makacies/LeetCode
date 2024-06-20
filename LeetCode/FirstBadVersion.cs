f6fc15908e8c81f64d991be9df19d2a8df5c7a61
makacies <barberrywork@gmail.com>
2024-06-20 21:48:19 +0300

    ﻿278. First Bad Version

---
+++ LeetCode/FirstBadVersion.cs
@@ -0,0 +1,34 @@
+﻿// 278. First Bad Version
+
+namespace LeetCode
+{
+    internal static class FirstBadVersion 
+    {
+        public static int Find(int n)
+        {
+            var start = 0;
+            var end = n;
+
+            while (start < end)
+            {
+                var mid = (end - start) / 2 + start;
+
+                if (IsBadVersion(mid))
+                {
+                    end = mid;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+
+            return start;
+        }
+
+        static bool IsBadVersion(int version)
+        {
+            return version >= 4;
+        }
+    }
+}
