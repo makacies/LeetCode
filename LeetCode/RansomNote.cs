1a220492f3b6bc57b4bad19d924b4861ee1ec5ab
makacies <barberrywork@gmail.com>
2024-07-18 20:02:18 +0300

    383. Ransom Note

---
+++ LeetCode/RansomNote.cs
@@ -0,0 +1,22 @@
+﻿// 383. Ransom Note
+
+namespace LeetCode
+{
+    internal static class RansomNote
+    {
+        internal static bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (magazine.Length != ransomNote.Length) return false;
+
+            var magazineSorted = magazine.ToCharArray().Order().ToList();
+            var noteSorted= ransomNote.ToCharArray().Order().ToList();
+
+            for (int i = 0; i < magazineSorted.Count; i++)
+            {
+                if (magazineSorted[i] != noteSorted[i]) return false;
+            }
+
+            return true;
+        }
+    }
+}
