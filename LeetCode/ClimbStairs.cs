da1a5d6982ae39b99ea68bebc846ea7cc5c02e7e
makacies <barberrywork@gmail.com>
2024-07-18 21:28:11 +0300

    70. Climbing Stairs

---
+++ LeetCode/ClimbStairs.cs
@@ -0,0 +1,22 @@
+﻿// 70. Climbing Stairs
+
+namespace LeetCode
+{
+    public static class ClimbStairs
+    {
+        public static int PossibleWays(int n)
+        {
+            var prev = 0;
+            var current = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                var temp = prev;
+                prev = current;
+                current += temp;
+            }
+
+            return current;
+        }
+    }
+}
