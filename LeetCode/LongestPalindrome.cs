ff6d3d473288d78c7f37d4c6919915e7c7ae40d3
makacies <barberrywork@gmail.com>
2024-07-19 19:08:23 +0300

    409. Longest Palindrome

---
+++ LeetCode/LongestPalindrome.cs
@@ -0,0 +1,27 @@
+﻿// 409. Longest Palindrome
+
+namespace LeetCode
+{
+    public class LongestPalindrome
+    {
+        public int GetLenght(string s)
+        {
+            var set = new HashSet<char>();
+            var length = 0;
+
+            foreach (char c in s)
+            {
+                if (set.Remove(c))
+                {
+                    length += 2;
+                }
+                else
+                {
+                    set.Add(c);
+                }
+            }
+
+            return set.Count() > 0 ? ++ length : length;
+        }
+    }
+}
