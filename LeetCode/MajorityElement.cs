1374c41d5a72e29cbf1a8b058a7c93ec9d9859a5
makacies <barberrywork@gmail.com>
2024-08-02 23:02:12 +0300

    ﻿// 169. Majority Element

---
+++ LeetCode/MajorityElement.cs
@@ -0,0 +1,50 @@
+﻿// 169. Majority Element
+
+namespace LeetCode
+{
+    internal static class MajorityElement
+    {
+        public static int GetMajorityElement(int[] nums)
+        {
+            // First pass: find a potential candidate for the majority element
+            int candidate = nums[0];
+            int count = 1;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] == candidate)
+                {
+                    count++;
+                }
+                else
+                {
+                    count--;
+                    if (count == 0)
+                    {
+                        candidate = nums[i];
+                        count = 1;
+                    }
+                }
+            }
+
+            // Second pass: verify the candidate
+            count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == candidate)
+                {
+                    count++;
+                }
+            }
+
+            if (count > nums.Length / 2)
+            {
+                return candidate;
+            }
+            else
+            {
+                throw new InvalidOperationException("No majority element found");
+            }
+        }
+    }
+}
