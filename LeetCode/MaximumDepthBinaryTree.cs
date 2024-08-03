9a2001a19b5d728da3446c9c1721bd8df94c73e7
makacies <barberrywork@gmail.com>
2024-08-03 21:24:36 +0300

    104. Maximum Depth of Binary Tree

---
+++ LeetCode/MaximumDepthBinaryTree.cs
@@ -0,0 +1,17 @@
+﻿// 104.Maximum Depth of Binary Tree
+
+namespace LeetCode
+{
+    internal class MaximumDepthBinaryTree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var left = MaxDepth(root.left);
+            var right = MaxDepth(root.right);
+
+            return left >= right ? ++left : ++right;
+        }
+    }
+}
