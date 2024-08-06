de4ef205fd13817bdbee3d44e07f834efc7a135b
makacies <barberrywork@gmail.com>
2024-08-06 23:09:25 +0300

    217. Contains Duplicate

---
+++ LeetCode/ContainsDuplicate.cs
@@ -0,0 +1,20 @@
+﻿// 217. Contains Duplicate
+
+namespace LeetCode
+{
+    internal class ContainsDuplicate
+    {
+        internal bool Contains(int[] nums)
+        {
+            Array.Sort(nums);
+            var previous = nums[0] - 1;
+            foreach (int i in nums)
+            {
+                if (i == previous) return true;
+                else previous = i;
+            }
+
+            return false;
+        }
+    }
+}
