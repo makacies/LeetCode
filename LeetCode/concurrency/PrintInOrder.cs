8ddd520e3b75f734033598271786cd7f31f10d03
makacies <barberrywork@gmail.com>
2024-08-07 20:16:24 +0300

    Update PrintInOrder.cs

--- LeetCode/concurrency/PrintInOrder.cs
+++ LeetCode/concurrency/PrintInOrder.cs
@@ -1,6 +1,38 @@
-﻿namespace LeetCode.concurrency
+﻿// 1114. Print in Order
+
+namespace LeetCode.concurrency
 {
-    internal class Class1
+    internal class PrintInOrder
     {
+        private ManualResetEventSlim _eventSlimFirst;
+        private ManualResetEventSlim _eventSlimSecond;
+
+        public PrintInOrder()
+        {
+            _eventSlimFirst = new ManualResetEventSlim(false);
+            _eventSlimSecond = new ManualResetEventSlim(false);
+        }
+
+        public void First(Action printFirst)
+        {
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+            _eventSlimFirst.Set();
+        }
+
+        public void Second(Action printSecond)
+        {
+            _eventSlimFirst.Wait();
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+            _eventSlimSecond.Set();
+        }
+
+        public void Third(Action printThird)
+        {
+            _eventSlimSecond.Wait();
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+        }
     }
 }
