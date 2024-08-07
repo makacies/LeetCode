f665db131cad232b660595c5e5ff8ff0325be538
makacies <barberrywork@gmail.com>
2024-08-07 12:41:19 +0300

    53. Maximum Subarray

---
+++ LeetCode/MaximumSubarray.cs
@@ -0,0 +1,26 @@
+﻿// 53. Maximum Subarray
+
+namespace LeetCode
+{
+    internal static class MaximumSubarray
+    {
+        public static int MaxSubArray(int[] nums)
+        {
+            var max = nums[0];
+            var sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+                if (nums[i] > sum)
+                {
+                    sum = nums[i];
+                }
+                if (sum > max)
+                {
+                    max = sum;
+                }
+            }
+            return max;
+        }
+    }
+}
