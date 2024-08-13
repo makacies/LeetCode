ba10e750ce4b05598e77c670417949f17ef6a66f
makacies <barberrywork@gmail.com>
2024-08-13 18:56:54 +0300

    1117. Building H2O

---
+++ LeetCode/concurrency/BuildingH2O.cs
@@ -0,0 +1,34 @@
+﻿// 1117. Building H2O
+
+namespace LeetCode.concurrency
+{
+    internal class BuildingH2O
+    {
+        private Semaphore _hydrogen;
+        private Semaphore _oxygen;
+        private Barrier _barrier;
+
+        public BuildingH2O()
+        {
+            _hydrogen = new Semaphore(2, 2);
+            _oxygen = new Semaphore(1, 1);
+            _barrier = new Barrier(3);
+        }
+
+        public void Hydrogen(Action releaseHydrogen)
+        {
+            _hydrogen.WaitOne();
+            releaseHydrogen();
+            _barrier.SignalAndWait();
+            _oxygen.Release();
+        }
+
+        public void Oxygen(Action releaseOxygen)
+        {
+            _oxygen.WaitOne();
+            releaseOxygen();
+            _barrier.SignalAndWait();
+            _hydrogen.Release();
+        }
+    }
+}
