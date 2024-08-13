94caefa9a6f7d88fd8de6b650f64c8beac81142b
makacies <barberrywork@gmail.com>
2024-08-13 13:25:58 +0300

    338. Counting Bits

---
+++ LeetCode/algorithms/CountingBits.cs
@@ -0,0 +1,18 @@
+﻿// 338. Counting Bits
+
+namespace LeetCode.algorithms
+{
+    internal static class CountingBits
+    {
+        public static int[] CountBits(int n)
+        {
+            var ans = new int[n + 1];
+            for (int i = 0; i < ans.Length; i++)
+            {
+                ans[i] = ans[i >> 1] + (i & 1);
+            }
+
+            return ans;
+        }
+    }
+}
