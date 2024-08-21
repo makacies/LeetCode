acf46b82d7b7c6c4039bcef0c44fe5a58240654a
makacies <barberrywork@gmail.com>
2024-08-21 18:03:42 +0300

    283. Move Zeroes

---
+++ LeetCode/algorithms/MoveZeroes.cs
@@ -0,0 +1,28 @@
+﻿// 283. Move Zeroes
+
+namespace LeetCode.algorithms
+{
+    internal class MoveZeroes
+    {
+        public void Move(int[] nums)
+        {
+            if (nums.Length == 1) return;
+            var indices = new Queue<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0) indices.Enqueue(i);
+                else
+                {
+                    if (indices.Count > 0)
+                    {
+                        var zeroIndex = indices.Dequeue();
+                        nums[zeroIndex] = nums[i];
+                        nums[i] = 0;
+                        indices.Enqueue(i);
+                    }
+                }
+            }
+        }
+    }
+}
